5ca8d8610303a04f9af47c329877c2c7d9fedd10
Robert <fishwickrob@live.com>
2018-05-22 10:50:30 +0100

    Installed pug and got the views working

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'pug');
 app.listen(3000, () => console.log('example app listening on port 3000!'));
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.render('index', { title: 'Hey', message: 'Hello World' }));
