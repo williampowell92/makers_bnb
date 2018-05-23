acd03bb492540517578d63301d06b17053944978
Igor Ryabchuk <riabtchuk@gmail.com>
2018-05-23 15:56:10 +0100

    extracts database query into separate module

--- src/exampleForTesting.js
+++ src/exampleForTesting.js
@@ -1,4 +1,16 @@
-var Calc = function (a) {
-  a++
-  return a
+module.exports = {
+
+  allRows: async function () {
+    const db = require('../server/models/index')
+    this.SPACES = db.Spaces
+    var array = []
+    await this.SPACES
+      .findAll()
+      .then(function (spaces) {
+        spaces.forEach(function (space) {
+          array.push(space.get('name'))
+        })
+      })
+    return array
+  }
 }
