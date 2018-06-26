a8e885efa854d5d05b71777d9e61d79f97740fe4
William Powell <william.powell92@gmail.com>
2018-06-26 22:36:16 +0100

    Linted datePickerScript

--- public/js/datePickerScript.js
+++ public/js/datePickerScript.js
@@ -1,26 +1,29 @@
-$( function() {
-  var dateFormat = "dd/mm/yyyy",
-    from = $( ".input_field__startDate" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 1
-    }).on( "change", function() {
-      to.datepicker( "option", "minDate", getDate( this ) );
-    }),
-    to = $( ".input_field__endDate" ).datepicker({
-      defaultDate: "+1w",
-      changeMonth: true,
-      numberOfMonths: 1
-    }).on( "change", function() {
-      from.datepicker( "option", "maxDate", getDate( this ) );
-    });
-  function getDate( element ) {
-    var date;
+$(() => {
+  const dateFormat = 'dd/mm/yyyy';
+
+  function getDate(element) {
+    let date;
     try {
-      date = $.datepicker.parseDate( dateFormat, element.value );
-    } catch( error ) {
+      date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
       date = null;
     }
     return date;
   }
+
+  const to = $('.input_field__endDate').datepicker({
+    defaultDate: '+1w',
+    changeMonth: true,
+    numberOfMonths: 1
+  }).on('change', () => {
+    from.datepicker('option', 'maxDate', getDate(this));
+  });
+
+  const from = $('.input_field__startDate').datepicker({
+    defaultDate: '+1w',
+    changeMonth: true,
+    numberOfMonths: 1
+  }).on('change', () => {
+    to.datepicker('option', 'minDate', getDate(this));
+  });
 });
